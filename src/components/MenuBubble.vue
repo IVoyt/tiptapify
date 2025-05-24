c591f5e873538a115d5e66dfb3b10f41b419f8ee
igorv <i.voitovych@liga.net>
2025-05-25 01:11:22 +0300

    MenuBubble refactor & extend actions

--- src/components/MenuBubble.vue
+++ src/components/MenuBubble.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import LinkDialog from "@tiptapify/components/extensions/components/LinkDialog.vue";
 import { useEditor } from "@tiptapify/composable/useEditor";
-import { defineProps, ref } from "vue";
+import { computed, defineProps, ref } from "vue";
 import { BubbleMenu } from '@tiptap/vue-3/menus'
 import * as mdi from '@mdi/js'
 
@@ -13,6 +13,69 @@ const { editor } = useEditor()
 const editorInstance = ref(editor.getInstance())
 
 const bubbleMenuLinkButton = ref(null)
+
+const items = ref([
+  {
+    name: 'bold',
+    icon: mdi.mdiFormatBold,
+    props: {
+      active: false,
+      disabled: computed(() => !editorInstance.value.can().chain().focus().toggleBold().run()),
+      color: computed(() => editorInstance.value.isActive('bold') ? 'primary' : ''),
+    },
+    click: () => editorInstance.value.chain().focus().toggleBold().run(),
+  },
+  {
+    name: 'italic',
+    icon: mdi.mdiFormatItalic,
+    props: {
+      active: false,
+      disabled: computed(() => !editorInstance.value.can().chain().focus().toggleItalic().run()),
+      color: computed(() => editorInstance.value.isActive('italic') ? 'primary' : ''),
+    },
+    click: () => editorInstance.value.chain().focus().toggleItalic().run(),
+  },
+  {
+    name: 'strike',
+    icon: mdi.mdiFormatStrikethroughVariant,
+    props: {
+      active: false,
+      disabled: computed(() => !editorInstance.value.can().chain().focus().toggleStrike().run()),
+      color: computed(() => editorInstance.value.isActive('strike') ? 'primary' : ''),
+    },
+    click: () => editorInstance.value.chain().focus().toggleStrike().run(),
+  },
+  {
+    name: 'underline',
+    icon: mdi.mdiFormatUnderline,
+    props: {
+      active: false,
+      disabled: computed(() => !editorInstance.value.can().chain().focus().toggleUnderline().run()),
+      color: computed(() => editorInstance.value.isActive('underline') ? 'primary' : ''),
+    },
+    click: () => editorInstance.value.chain().focus().toggleUnderline().run(),
+  },
+  {
+    name: 'highlight',
+    icon: mdi.mdiFormatColorHighlight,
+    props: {
+      active: false,
+      disabled: computed(() => !editorInstance.value.can().chain().focus().toggleHighlight().run()),
+      color: computed(() => editorInstance.value.isActive('highlight') ? 'primary' : ''),
+    },
+    click: () => editorInstance.value.chain().focus().toggleHighlight().run(),
+  },
+  {
+    name: 'link',
+    icon: computed(() => editorInstance.value.isActive('link') ? mdi.mdiLinkOff : mdi.mdiLink),
+    props: {
+      active: false,
+      color: computed(() => editorInstance.value.isActive('link') ? 'primary' : ''),
+      disabled: computed(() => editorInstance.value.isActive('code') || editorInstance.value.isActive('codeBlock')),
+    },
+    click: () => linkAction(),
+  }
+])
 
 function linkAction() {
   return editorInstance.value.isActive('link')
@@ -26,29 +89,8 @@ function linkAction() {
   <BubbleMenu v-if="editorInstance" :editor="editorInstance">
     <div class="bubble-menu">
       <VBtnToggle divided density="compact" :variant="variant">
-        <VBtn
-            @click="editorInstance.chain().focus().toggleBold().run()"
-            :active="editorInstance.isActive('bold')"
-            size="small"
-        >
-          <VIcon :icon="mdi.mdiFormatBold" size="16" />
-        </VBtn>
-        <VBtn
-            @click="editorInstance.chain().focus().toggleItalic().run()"
-            :active="editorInstance.isActive('italic')"
-            size="small"
-        >
-          <VIcon :icon="mdi.mdiFormatItalic" size="16" />
-        </VBtn>
-        <VBtn
-            @click="editorInstance.chain().focus().toggleStrike().run()"
-            :active="editorInstance.isActive('strike')"
-            size="small"
-        >
-          <VIcon :icon="mdi.mdiFormatStrikethroughVariant" size="16" />
-        </VBtn>
-        <VBtn @click="linkAction" :active="editorInstance.isActive('link')" size="small">
-          <VIcon :icon="`${ editorInstance.isActive('link') ? mdi.mdiLinkOff : mdi.mdiLink}`" size="16" />
+        <VBtn v-for="(item, key) in items" :key="key" v-bind="item.props" @click="item.click" size="small">
+          <VIcon :icon="item.icon" size="16" />
         </VBtn>
       </VBtnToggle>
     </div>
