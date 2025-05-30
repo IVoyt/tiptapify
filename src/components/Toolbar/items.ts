5b4546c835cdbfb75093896d60034673d955790d
igorv <i.voitovych@liga.net>
2025-05-31 01:39:02 +0300

    refactor

--- src/components/Toolbar/items.ts
+++ src/components/Toolbar/items.ts
@@ -1,124 +1,6 @@
 import { computed, ComputedRef, Ref, ref } from "vue";
 import * as mdi from '@mdi/js'
-
-const fonts = ref([
-  {
-    name: 'Arial',
-    fontFamily: 'arial'
-  },
-  {
-    name: 'Arial Black',
-    fontFamily: 'arial black'
-  },
-  {
-    name: 'Baskerville',
-    fontFamily: 'baskerville'
-  },
-  {
-    name: 'Bodoni MT',
-    fontFamily: 'bodoni mt'
-  },
-  {
-    name: 'Brush Script MT',
-    fontFamily: 'brush script mt'
-  },
-  {
-    name: 'Calibri',
-    fontFamily: 'calibri'
-  },
-  {
-    name: 'Calisto MT',
-    fontFamily: 'calisto mt'
-  },
-  {
-    name: 'Cambria',
-    fontFamily: 'cambria'
-  },
-  {
-    name: 'Century Gothic',
-    fontFamily: 'century gothic'
-  },
-  {
-    name: 'Consolas',
-    fontFamily: 'consolas'
-  },
-  {
-    name: 'Comic Sans',
-    fontFamily: 'comic sans ms, comic sans'
-  },
-  {
-    name: 'Courier',
-    fontFamily: 'Courier'
-  },
-  {
-    name: 'Courier New',
-    fontFamily: 'courier new'
-  },
-  {
-    name: 'Cursive',
-    fontFamily: 'cursive'
-  },
-  {
-    name: 'Dejavu Sans',
-    fontFamily: 'dejavu sans'
-  },
-  {
-    name: 'Franklin Gothic',
-    fontFamily: 'franklin gothic'
-  },
-  {
-    name: 'Garamond',
-    fontFamily: 'garamond'
-  },
-  {
-    name: 'Georgia',
-    fontFamily: 'georgia'
-  },
-  {
-    name: 'Helvetica',
-    fontFamily: 'helvetica'
-  },
-  {
-    name: 'Impact',
-    fontFamily: 'impact'
-  },
-  {
-    name: 'Inter',
-    fontFamily: 'inter'
-  },
-  {
-    name: 'Monospace',
-    fontFamily: 'monospace'
-  },
-  {
-    name: 'Optima',
-    fontFamily: 'optima'
-  },
-  {
-    name: 'Segoe UI',
-    fontFamily: 'segoe ui'
-  },
-  {
-    name: 'Serif',
-    fontFamily: 'serif'
-  },
-  {
-    name: 'Tahoma',
-    fontFamily: 'tahoma'
-  },
-  {
-    name: 'Time New Roman',
-    fontFamily: 'times new roman'
-  },
-  {
-    name: 'Trebuchet MS',
-    fontFamily: 'trebuchet ms'
-  },
-  {
-    name: 'Verdana',
-    fontFamily: 'verdana'
-  },
-])
+import { fonts } from './fonts'
 
 interface MDIIcons {
   [key: string]: string
@@ -184,7 +66,6 @@ export function toolbarItems(
      * font color, backgroundcolor
      * tables
      * media (image, video)
-     * unsetmarks, clearnodes
      */
     heading: {
       name: 'heading',
@@ -227,7 +108,7 @@ export function toolbarItems(
       attrs: {
         click: () => editor.value.chain().focus().unsetFontFamily().run()
       },
-      children: fonts.value.map((font) => {
+      children: fonts.map((font) => {
         return {
           name: font.name,
           tooltip: '',
